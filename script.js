31abe823f338c2d9f58b5932930c9c9c0b1c045c
Egi Munandar <lucksss22222@gmail.com>
2025-11-15 11:03:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 const contacts = [
     {
         id: 1,
-        name: "Egi Munandar",
+        fullname: "Egi Munandar",
         phone: "80958809848",
         email: "egi@exemple.com",
         location: "Sambas",
     },
     {
         id: 2,
-        name: "Debsa Tauludin",
+        fullname: "Debsa Tauludin",
         phone: "808391858769",
         email: "debsa@exemple.com",
         location: "Sambas",
     },    
     {
         id: 3,
-        name: "Filda Parwita",
+        fullname: "Filda Parwita",
         phone: "8081074756839",
         email: "Filda@exemple.com",
         location: "Sambas",
     },    
     {
         id: 4,
-        name: "Adityanda",
+        fullname: "Adityanda",
         phone: "8087137410593",
         email: "adtiya@exmple.com",
         location: "Sambas",
     },
     {
         id: 5,
-        name: "Devin",
+        fullname: "Devin",
         phone: "809172487598",
         email: "Devin@exemple.com",
         location: "Jakarta",
     },
 ]
+
+console.table(contacts);
